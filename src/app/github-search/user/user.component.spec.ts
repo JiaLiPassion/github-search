5a5ae827e10008b5bcf45037a9e2669204f15a0d
JiaLiPassion <JiaLi.Passion@gmail.com>
2019-08-21 16:13:31 +0900

    fix: update style and add test cases for user component

--- src/app/github-search/user/user.component.spec.ts
+++ src/app/github-search/user/user.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UserComponent } from './user.component';
+import { User, GitHubCategory } from '../../github-client';
+import { expectElementMatchUrl, expectElementContainText } from '../testing/component.test.util';
 
 describe('UserComponent', () => {
   let component: UserComponent;
@@ -8,9 +10,8 @@ describe('UserComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ UserComponent ]
-    })
-    .compileComponents();
+      declarations: [UserComponent]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
@@ -21,5 +22,27 @@ describe('UserComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  const testDatas: User[] = [
+    {
+      login: 'testRepo',
+      githubUrl: 'http://test/',
+      type: GitHubCategory.Users,
+      avatarUrl: 'http://avatar',
+      followerCount: 10,
+      starredCount: 20
+    }
+  ];
+
+  testDatas.forEach(testData => {
+    it('should display User data correctly', () => {
+      component.item = testData;
+      fixture.detectChanges();
+      expectElementContainText(fixture, '.handle', testData.login);
+      expectElementContainText(fixture, '.star', testData.starredCount.toString());
+      expectElementContainText(fixture, '.follower', testData.followerCount.toString());
+      expectElementMatchUrl(fixture, '.avatar > a', testData.avatarUrl);
+    });
   });
 });
